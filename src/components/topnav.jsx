8d2ba7aa2b42367ff1813b916c768bc77c55176e
Doyller <117834332+Doyller@users.noreply.github.com>
2023-04-06 18:00:54 -0230

    update

---
+++ src/components/topnav.jsx
@@ -0,0 +1,21 @@
+/*const topNav = () => {
+  return (
+    <div className="topNav">
+      <li>
+        <Link to="/">Billy Bobs Tech for Less</Link>
+      </li>
+      <li>
+        <Link to="/">Top Deals</Link>
+      </li>
+      <li>
+        <Link to="/">Computers</Link>
+      </li>
+      <li>
+        <Link to="/">Cellphones</Link>
+      </li>
+
+    </div>
+  );
+};
+
+export default topNav*/
