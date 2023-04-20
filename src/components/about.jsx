d76274f57f9f07e88331318f7210c06151c56377
ShovelKnight11 <riley.ledrew@keyin.com>
2023-04-19 23:04:19 -0230

    Finished about page

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 
 function About() {
   return (
-    <div>
+    <><div>
       <h1>About Us</h1>
       <br />
       <h2>About Billy</h2>
@@ -13,7 +13,11 @@ function About() {
         crowd. That's why Billy Bob's Tech Sales prioritizes customer experience
         over all else. We are here for you.
       </p>
-    </div>
+    </div>\
+    <div>
+        <h1>Contact Us</h1>
+        <div><img src="images/MicrosoftTeams-image (1).png" alt="na" className="chatimg" /><h3 className="chatimgtxt">Never Fear, <br /> Billy is Here! <br /> Call Anytime <br /> 123-456-7890</h3></div>
+      </div></>
   );
 }
 export default About;
